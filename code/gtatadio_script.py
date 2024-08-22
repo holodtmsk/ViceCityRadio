89bf58bb2ae5d49b4b404d8a6a24b122cba6d0cc
holodtmsk <holod.tmsk@gmail.com>
2024-08-22 23:32:21 +0700

    Update gtatadio_script.py

--- code/gtatadio_script.py
+++ code/gtatadio_script.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Вставьте ваш токен здесь
 BOT_TOKEN = '7503606129:AAEVHZPaRJhwRsPfAs2XrFDjybDSqHaS9_w'
 
+# Создание Flask приложения
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,22 +13,28 @@ def index():
     # Это будет загружать файл index.html из директории templates
     return render_template('index.html')
 
-async def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Создаем кнопку, которая откроет ваше веб-приложение
-    button = InlineKeyboardButton("Open Web App", web_app={"url": "https://instagram-bot22-1d84ba019e98.herokuapp.com"})
+    button = InlineKeyboardButton("Open Web App", url="https://instagram-bot22-1d84ba019e98.herokuapp.com")
     keyboard = InlineKeyboardMarkup([[button]])
 
     await update.message.reply_text('Click the button to open the app:', reply_markup=keyboard)
 
-# Создаем приложение Telegram Bot
-telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+# Создание и настройка Telegram Bot приложения
+def main():
+    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Добавляем обработчик команды /start
-telegram_app.add_handler(CommandHandler("start", start))
-
-if __name__ == '__main__':
-    # Запуск Flask приложения
-    app.run(debug=True)
+    # Добавляем обработчик команды /start
+    telegram_app.add_handler(CommandHandler("start", start))
 
     # Запуск Telegram бота
     telegram_app.run_polling()
+
+if __name__ == '__main__':
+    # Запуск Flask приложения
+    app.run(debug=True, use_reloader=False)  # Добавлено use_reloader=False для предотвращения двойного запуска
+
+    # Запуск Telegram бота
+    main()
+
+
