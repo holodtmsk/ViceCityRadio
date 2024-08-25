b48c410765017fe7ef2bba227173b3f8bc927d44
holodtmsk <holod.tmsk@gmail.com>
2024-08-25 23:46:19 +0700

    Update gtatadio_script.py

--- code/gtatadio_script.py
+++ code/gtatadio_script.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from threading import Thread
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext, ContextTypes
 
-# Ваш токен Telegram бота
+# Вставьте ваш токен здесь
 BOT_TOKEN = '7503606129:AAEVHZPaRJhwRsPfAs2XrFDjybDSqHaS9_w'
 
 # Создание Flask приложения
@@ -12,32 +10,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Это будет загружать файл index.html из директории templates
-    return render_template('index.html')
+    return render_template('index.html', 
+                           emotion_url='https://disk.yandex.ru/d/U3nGEWXN06uuYg',
+                           vrock_url='https://disk.yandex.ru/d/NSofbwMSIUqSdA',
+                           wave_url='https://disk.yandex.ru/d/WjhlOGNdtCbA5w')
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Создаем кнопку, которая откроет ваше веб-приложение
     button = InlineKeyboardButton("Open Web App", url="https://instagram-bot22.herokuapp.com")
     keyboard = InlineKeyboardMarkup([[button]])
 
     await update.message.reply_text('Click the button to open the app:', reply_markup=keyboard)
 
-# Создание и настройка Telegram Bot приложения
-def run_telegram_bot():
-    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+# Создаем приложение Telegram Bot
+telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    # Добавляем обработчик команды /start
-    telegram_app.add_handler(CommandHandler("start", start))
+# Добавляем обработчик команды /start
+telegram_app.add_handler(CommandHandler("start", start))
+
+if __name__ == '__main__':
+    # Запуск Flask приложения
+    app.run(debug=True, use_reloader=False)
 
     # Запуск Telegram бота
     telegram_app.run_polling()
-
-if __name__ == '__main__':
-    # Запуск Telegram бота в отдельном потоке
-    bot_thread = Thread(target=run_telegram_bot)
-    bot_thread.start()
-
-    # Запуск Flask приложения
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), use_reloader=False)
-
 
