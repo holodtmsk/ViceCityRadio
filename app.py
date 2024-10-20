960600ef15eada39fa4e4fbb8b22bf4a2c3e55b8
holodtmsk <holod.tmsk@gmail.com>
2024-10-20 18:13:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import sqlite3
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Heroku!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 # Инициализация базы данных
 def init_db():
